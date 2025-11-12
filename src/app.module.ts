8f7ff8ac56caf78c3d5fd8cdfd421a0a514af54e
matshp0 <matvii.shpyruk@gmail.com>
2025-11-12 18:16:39 +0200

    feat: add config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import configSchema from './config/config.schema';
+import config from './config/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [config],
+      validationSchema: configSchema,
+    }),
+  ],
 })
 export class AppModule {}
---
+++ src/config/config.schema.ts
@@ -0,0 +1,8 @@
+import * as Joi from 'joi';
+
+export default Joi.object({
+  NODE_ENV: Joi.string()
+    .valid('development', 'production', 'test', 'provision')
+    .default('development'),
+  PORT: Joi.number().port().default(3000),
+});
